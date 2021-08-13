a7173965a001c5c36940c7d6a9656e9407473baf
Júlia Francisquini Fritegotto <47829365+juliafritegotto@users.noreply.github.com>
2021-08-13 13:55:48 -0300

    menu lateral estilizado e com rotas

---
+++ frontpt/src/components/SidebarData.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Novo Ponto Turístico',
+    path: '/pontos',
+    icon: <FaIcons.FaPlus />,
+    cName: 'nav-text'
+  }
+];
---
+++ frontpt/src/pages/AddPonto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddPonto() {
+  return (
+    <div className='pontos'>
+      <h1>Novo Ponto Turístico</h1>
+    </div>
+  );
+}
+
+export default AddPonto;
